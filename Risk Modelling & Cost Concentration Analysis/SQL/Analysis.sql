25aae1005d7950580fa9cba2b9438b56e9662444
Rohith Geddam <rohithgeddam02@gmail.com>
2026-02-01 23:34:28 +0530

    Create Analysis.sql
    Added SQL queries for cost analysis by risk category and Pareto preparation.

---
+++ Risk Modelling & Cost Concentration Analysis/SQL/Analysis.sql
@@ -0,0 +1,13 @@
+-- Cost by risk category
+SELECT
+  risk_category,
+  SUM(claim_amount) AS total_cost
+FROM Insurance_claims_cleaned_dataset
+GROUP BY risk_category;
+
+-- Pareto preparation
+SELECT
+  claim_amount,
+  SUM(claim_amount) OVER (ORDER BY claim_amount DESC)
+  / SUM(claim_amount) OVER () AS cumulative_cost_pct
+FROM Insurance_claims_cleaned_dataset;
